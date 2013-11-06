d7638e9f26ee9121ff412fd2ffa08052b29ee687
weiwei19890607 <weiwei19890607@hotmail.com>
2013-11-07 00:18:36 +0800

    pinkpoint added

---
+++ LeapMotion_Pointing/Assets/scripts/PointController.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PointController : MonoBehaviour {
+	
+	public Texture2D icon;
+
+	void OnGUI () {
+		GUI.DrawTexture (new Rect (0,0, 50, 50), icon);
+			
+	}
+	
+}
